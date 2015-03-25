4addffa1e0f1469d52e106abb8a8c21883917be5
jguang <jguang001@gmail.com>
2015-03-26 00:36:44 +0800

    增加 Item 基类

--- src/widgets/Style.js
+++ src/widgets/Style.js
@@ -10,16 +10,38 @@ define([
     '../core/lib'
 ], function (Class, nativeApi, lib) {
 
-    // 统一处理颜色值;
+    /**
+     * 统一处理颜色值和透明度;
+     * @param {string} 颜色值支持 rgb(),rgba(),#xxx,#xxxxxx，如果是 rgba设置的 opcity无效；
+     * @param {number} 透明度0~1；
+     * @return {string} Native 支持的颜色值:#+透明度(00~ff)+6位16进制颜色值(000000~ffffff)；
+     */
     var _naitveColor = function (color, opacity) {
-        var opacity = opacity || 1;
-        var color = color || '#000000';
-        var color = color.indexOf('#')===0? color.slice(1) : color;
+        var colors;
+        opacity = opacity || 1;
         opacity = Math.ceil(opacity * 0xff).toString(16);
-        if(color.length === 3){
-            color = color[0]+color[0]+color[1]+color[1]+color[2]+color[2];
+        color = color || '#000000';
+        var toX = function ( str ){
+            return ('00' + Number(str).toString(16)).substr(-2);
+        };
+        if (color.indexOf('#') === 0) {
+            color = color.slice(1);
+            if (color.length === 3) {
+                color = color[0] + color[0] + color[1] + color[1] + color[2] + color[2];
+            }
         }
-        return  '#'+ opacity + color;
+        else if ((colors = color.match(/^[\s]*rgb[a]?\(([\,|\d]+)\)/i))) {
+            colors = colors[1].split(',');
+            if (colors[3]) {
+                opacity = toX(colors[3]);
+            }
+            color = '';
+            for (var i=0; i<3; i++ ) {
+                color += toX(colors[i]);
+            }
+        }
+
+        return '#' + opacity + color;
     };
 
 
@@ -40,21 +62,20 @@ define([
          * @return {Object} style实例；
          */
         init: function (options) {
-            var t = this;
             this.config = {};
             this._setOptions(options);
             return this;
         },
 
         // 绑定事件
-        events:{
-            // data 属性改变会触发进行解析极其触发render
-            'change': function (key) {
+        events: {
+            // data 属性改变会触发进行解析;
+            change: function (key) {
                 if (this['_parse' + lib.toPascal(key)]) {
-                    this['_parse' + lib.toPascal(key)]( key );
+                    this['_parse' + lib.toPascal(key)](key);
                 }
                 else {
-                    this.config[k] = this.get(k);
+                    this.config[key] = this.get(key);
                     console.log('还不支持此属性');
                 }
                 this.render();
@@ -62,21 +83,19 @@ define([
         },
 
         _setOptions: function (options) {
-            var data = options.data || {};
+            options = options || {};
+            var data = options.data;
             this.instance = options.instance;
             this.parentConfig = this.instance.config;
             this.parentConfig.style = this.config;
-            for(var k in data){
-                if (data.hasOwnProperty(k)) {
-                    this.set(k, data[k]);
-                }
-            }
+            data && this.update(data);
         },
 
         /**
          * 解析配置中的字体颜色
          * @private
          */
+
         _parseColor: function () {
             var color = this.get('color');
             var opacity = this.get('opacity');
@@ -95,6 +114,17 @@ define([
             return this;
         },
 
+        /**
+         * 更新 style的数据;
+         */
+        update： function (data) {
+            data = data || {};
+            for(var k in data){
+                if (data.hasOwnProperty(k)) {
+                    this.set(k, data[k]);
+                }
+            }
+        },
 
         /**
          * 渲染组件
