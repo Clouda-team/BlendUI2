286a57ff3cc48cdd5c3d4d108a7a2bd135aa34d2
jguang <jguang001@gmail.com>
2015-03-26 14:51:14 +0800

    修改 lib.js bug 和增加lib.js 自动化测试

--- test/autotest/lib.js
+++ test/autotest/lib.js
@@ -7,7 +7,7 @@ define(['../../src/core/lib'],function(lib){
             expect(lib.extend).to.be.a('function');
             expect(lib.noop).to.be.a('function');
         });
-        
+
         it('extend', function () {
             var s = lib.extend({},{
                 "aa": 'aaa',
@@ -16,8 +16,31 @@ define(['../../src/core/lib'],function(lib){
             expect(s).to.be.an('object')
             expect(s.aa).to.equal('aaa');
             expect(s.cc).to.equal('ccc');
+            lib.extend(s,{"dd":"ddd"});
+            expect(s.dd).to.equal('ddd');
         });
-        
-        
+
+        it('noop', function(){
+            expect(lib.noop).to.be.a('function');
+        });
+
+        it('toCamel', function(){
+            var as = "aa-bb";
+            as = lib.toCamel(as);
+            expect(as).to.equal('aaBb');
+            expect(lib.toCamel('aa-dd-cc-bb')).to.equal('aaDdCcBb');
+        });
+
+        it('toPascal', function(){
+            var as = "aa-bb";
+            as = lib.toPascal(as);
+            expect(as).to.equal('AaBb');
+            expect(lib.toPascal('aa-dd-cc-bb')).to.equal('AaDdCcBb');
+        });
+
+        // it('isUix', function(){
+        //     expect(lib.isUix).to.equal('AaDdCcBb');
+        // });
+
     });
 });
