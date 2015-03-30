7ec22bf6d72d6a0e4a66de34c08ba21761889579
dogemail@foxmail.com <dogemail@foxmail.com>
2015-03-30 11:31:47 +0800

    背景管理页新版更新

--- src/background/entrance.js
+++ src/background/entrance.js
@@ -1,155 +1,62 @@
 /**
-* 背景页面入口引导,用来管理WebView,为应用页面间通讯做代理
-*/
-(function(window){
-    //辅助类和方法
-    Array.prototype.indexOf = function(val) {
-        for (var i = 0; i < this.length; i++) {
-            if (this[i] == val) return i;
-        }
-        return -1;
+ * @file entrance.js
+ * @desc uix native背景页环境,维护应用上下文环境,
+ */
+(function () {
+    // 系统原生事件,新activity创建的时候触发webview_register事件, activity销毁的时候触发webview_unregister事件
+    // var _events = ['webview_register', 'webview_unregister'];
+    var bridge = lc_bridge;
+    var webViews = {};
+    var status = {
+        ACTIVE: 1,
+        PAUSED: 2,
+        STOPPED: 3,
+        KILLED: 0
     };
-
-    Array.prototype.remove = function(val) {
-        var index = this.indexOf(val);
-        if (index > -1) {
-            this.splice(index, 1);
-        }
-    };
-
-    //系统管理器
-    var Boost = function(config){
-        return new Boost.prototype.init(config);
-    };
-
-    var webviews = {
-        ids: [],
-        table: {},
-        register: function(id,state){
-            state = state || {};
-            if (!this.table[id]){
-                this.ids.push(id);
-            }
-            this.table[id] = state;
-        },
-        unregister: function(id){
-            delete this.table[id];
-            this.ids.remove(id);
-        }
-    };
-
-    var __event = {};
-
-    Boost.prototype = {
-        init: function(config){
-            this.config = config;
-            return this;
-        }
-    }
-
-    Boost.prototype.init.prototype = Boost.prototype;
-
-    Boost.prototype.load = function(id, data){
-        lc_bridge.exeJsOnContentWebview(id, data);
-    }
-
-    //WebView管理机制
-    Boost.prototype.register = function(){
-
-        document.addEventListener("webview_register", function(event){
-            var webview = JSON.parse(event.data);
-            webviews.register(webview.id);
+    // 监听activity webView创建事件
+    document.addEventListener('webview_register', function (event) {
+        (event.origin in webViews) || (webViews[event.origin] = {
+            origin: event.origin,
+            status: status.ACTIVE,
+            data: event.data
         });
-
-        document.addEventListener("webview_unregister", function(event){
-            var webview = JSON.parse(event.data);
-            webviews.unregister(webview.id);
-        });
-    }
-
-    //消息机制
-    Boost.prototype.delegate = function(){
-        var that = this;
-        document.addEventListener("delegate", function(event){
-            //业务页面通过背景页自我调用
-
-            lc_bridge.postMessage(event.origin, "message", event.origin);
-
-            var data = JSON.parse(event.data);
-
-            if (__event[data.type]){
-                for (var i in __event[data.type]){
-                    console.log("页面消息转发:"+data.type);
-                    lc_bridge.postMessage(__event[data.type][i], data.type, "页面ID"+event.origin+"|"+"内容:"+event.data);
-                }                
+    });
+    // 监听activity webView销毁事件
+    document.addEventListener('webview_unregister', function (event) {
+        (event.origin in webViews) && (delete webViews[event.origin]);
+    });
+    // 注册页面要监听的事件
+    var events = {};
+    document.addEventListener('event_register', function (event) {
+        var data = JSON.parse(event.data);
+        var type = data.type;
+        var origin = event.origin;
+        if (events[type]) {
+            events[type].indexOf(origin) < 0 || events[type].push(origin);
+        }
+        else {
+            events[type] = [origin];
+        }
+    }, false);
+    // 移除要监听的事件
+    document.addEventListener('event_unregister', function (event) {
+        var data = JSON.parse(event.data);
+        var type = data.type;
+        var origin = event.origin;
+        if (events[type]) {
+            var index = events[type].indexOf(origin);
+            events[type].splice(index, 1);
+        }
+    }, false);
+    // 触发事件
+    document.addEventListener('delegate', function (event) {
+        var data = JSON.parse(event.data);
+        var type = data.type;
+        var message = JSON.stringify(data.message);
+        if (events[type]) {
+            for (var i = 0, len = events[type].length; i < len; i++) {
+                bridge.postMessage(events[type][i], type, message);
             }
-
-        }, false);
-
-        document.addEventListener("event_register", function(event){
-            if (__event[event.data]){
-                __event[event.data].push(event.origin);
-            } else {
-                __event[event.data] = [event.origin];
-            }
-            console.log(event.origin);
-        }, false);
-
-        document.addEventListener("event_unregister", function(event){
-            __event[event.data] && __event[event.data].remove(event.origin);
-        }, false);
-    }
-
-    //用来测试的用例
-    Boost.prototype.test = function(){
-        document.addEventListener("test", function(event){
-            var data = JSON.parse(event.data);
-            console.log("test----");
-            if (data.type == "not-exist&post"){
-                console.log("not-exist&post");
-                lc_bridge.postMessage(event.data, "testCallback", data.data);
-                lc_bridge.postMessage(event.origin, "testCallback", data.data);
-            }
-
-            if (data.type == "not-exist&exec"){
-                console.log("not-exist&exec");
-                lc_bridge.exeJsRemote(event.data, "alert('exec')");
-                lc_bridge.exeJsRemote(event.origin, "alert('不存在的页面执行exec')");
-            }
-            
-        });
-    }    
-
-    Boost.prototype.run = function(){
-
-        //必需模块
-        this.register();
-
-        //消息通讯
-        this.delegate();
-
-        //测试用例
-        this.test();
-
-    }
-
-    Boost.webviews = webviews;
-
-    window.$app = window.Boost = Boost;
-
-    //模块化支持
-    if(typeof define !== undefined && typeof define.amd !== undefined){
-        define(function(){
-            return Boost;
-        });
-    }
-
-    //启动服务
-    var config = {
-        version: 1.0
-    };
-
-    $app(config).run();
-
-
-})(this);
+        }
+    }, false);
+})();
